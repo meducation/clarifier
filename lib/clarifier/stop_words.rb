3ee5ae1de64a945a77fa90f1691741a4f34b89b4
Rob Styles <rob.styles@dynamicorange.com>
2014-03-04 14:55:28 +0000

    Added generation of own stopword lists from training docs

--- lib/clarifier/stop_words.rb
+++ lib/clarifier/stop_words.rb
@@ -1,13 +1,22 @@
 module Clarifier
   class StopWords
 
+    attr_accessor :stopwords
+
     @@lists = {}
+
     def self.lists
       @@lists
     end
 
-    def initialize
-      @stopwords = @@lists[:en_gb_basic]
+    def initialize(desired_stopwords = nil)
+      if desired_stopwords.kind_of?(Array)
+        @stopwords = desired_stopwords
+      elsif @@lists[desired_stopwords]
+        @stopwords = @@lists[desired_stopwords]
+      else
+        @stopwords = @@lists[:en_gb_basic]
+      end
     end
 
     def clarify(input)
@@ -25,6 +34,23 @@ module Clarifier
       #new_string.gsub!(/\s+/, ' ')
 
       new_string
+    end
+
+    def train(docs, threshold = 0.8)
+      word_counts = {}
+      docs.each do |doc|
+        words = doc.split
+        words.uniq!
+        words.each do |word|
+          word_counts[word] ||= 0
+          word_counts[word] += 1
+        end
+      end
+      word_counts.each do |word,count|
+        if count.to_f / docs.length >= threshold
+          @stopwords << word
+        end
+      end
     end
 
   end
--- test/stop_words_test.rb
+++ test/stop_words_test.rb
@@ -1,20 +1,42 @@
 require_relative 'test_helper'
+require_relative 'test_docs'
 
-module MESH
-  describe 'Testing Clarifier::StopWords' do
+module Clarifier
 
-    it 'should use a default English stopword set' do
+  class ClarifierStopWordsTest < Minitest::Test
+
+
+    def test_use_default_english_stopword_set
       sw = Clarifier::StopWords.new
       input = 'The quick brown fox jumped over the lazy dog.'
       expected = 'quick brown fox jumped over lazy dog.'
       assert_equal expected, sw.clarify(input)
     end
 
-    it 'should derive a stopword list from a document set' do
-
+    def test_use_the_selected_stopword_list
+      skip
     end
 
+    def test_use_an_array_of_stopwords_if_provided
+      sw = Clarifier::StopWords.new(%w(quick brown lazy))
+      input = 'The quick brown fox jumped over the lazy dog.'
+      expected = 'The fox jumped over the dog.'
+      assert_equal expected, sw.clarify(input)
+    end
+
+    def test_use_an_empty_array_of_stopwords_if_provided
+      sw = Clarifier::StopWords.new([])
+      input = 'The quick brown fox jumped over the lazy dog.'
+      expected = 'The quick brown fox jumped over the lazy dog.'
+      assert_equal expected, sw.clarify(input)
+    end
+
+    def test_derive_a_stopword_list_from_a_document_set
+      sw = Clarifier::StopWords.new([])
+      sw.train(StopWordsTestDocs.docs)
+      expected = %w(we are to the for a of and then is at one time with this that have here or in which can by you it these when on so your has be not if more some as there they)
+      assert_equal expected, sw.stopwords
+    end
 
   end
-
 end
